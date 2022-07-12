36662f7ee2608a57581fb20f569f8cbf193c20a2
Parker Rowe <parker.rowe@ericsson.com>
2022-07-12 15:49:37 -0400

    vite build fix pt2

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,17 +11,6 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
-		vite: {
-			resolve: {
-				alias: {
-					'$components': path.resolve('src/components'),
-					'$styles': path.resolve('src/styles'),
-					'$data': path.resolve('data'),
-					'$types': path.resolve('src/types'),
-				}
-			}
-		}
 	},
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from "path";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      '$components': path.resolve('src/components'),
+      '$styles': path.resolve('src/styles'),
+      '$data': path.resolve('data'),
+      '$types': path.resolve('src/types'),
+    }
+  }
 };
 
 export default config;
